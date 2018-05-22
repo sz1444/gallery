b5632eb255afff2bc697594678f87b2b4b79aca6
Arkadiusz Wierzbicki <sz144@wp.pl>
2018-05-22 16:28:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 new Vue({
-  el: '#app',
-  data: {
-    items: [],
-    imgSrc: ' ',
-    imgTitle:' '  
-  },
-  mounted() {
-    axios.get("https://5b02ff4820848e001432ca08.mockapi.io/img/img")
-    .then(response => {this.items = response.data})
-  },
+    el: '#app',
+    data: {
+        items: [],
+        error: ' ',
+        imgSrc: ' ',
+        imgTitle: ' ',
+        nextImg: '',
+        nextTitle: '',
+        id: ''
+    },
+    mounted() {
+        axios.get("https://5b02ff4820848e001432ca08.mockapi.io/img/img")
+            .then(response => {
+                this.items = response.data
+            })
+    },
     methods: {
-    greet: function (event) {
-    this.imgSrc=event.target.src
-     this.imgTitle=event.target.alt    
+        greet: function (index) {
+            this.imgSrc = this.items[index].url
+            this.imgTitle = this.items[index].name
+            this.nextImg = this.items[index + 1].url
+            this.id = index
+        },
+        next: function () {
+            this.id=this.id+1
+            document.querySelector('.modal-body img').src= this.items[this.id].url 
+            console.log(this.id)
+        }
     }
-}
-    
-});
 
+});
